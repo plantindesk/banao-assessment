8e8350ffb2c019393569afd4f62cd79601d72856
plantindesk <t19.thakur@outlook.com>
2025-12-16 23:11:48 +0530

    feat: Add admin interface for user management

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (UserAdmin.fieldsets or ()) + (
+        ('Additional Information', {'fields': ('role', 'phone_number', 'date_of_birth')}),
+    )  # type: ignore[override]
+
+    add_fieldsets = (UserAdmin.add_fieldsets or ()) + (
+        ('Additional Information', {'fields': ('role', 'phone_number', 'date_of_birth')}),
+    )  # type: ignore[override]
+
+    list_display = (UserAdmin.list_display or ()) + ('role', 'phone_number')  # type: ignore[override]
+    
+    list_filter = (UserAdmin.list_filter or ()) + ('role',)  # type: ignore[override]
+
+admin.site.register(User, CustomUserAdmin)
