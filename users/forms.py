1c80c836d01cb6e8df56937bd65f1495cd0599f9
plantindesk <t19.thakur@outlook.com>
2025-12-16 23:12:30 +0530

    feat: Add forms for user registration and authentication

---
+++ users/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .models import User
+
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    first_name = forms.CharField(max_length=30, required=True)
+    last_name = forms.CharField(max_length=30, required=True)
+    role = forms.ChoiceField(choices=User.ROLE_CHOICES, required=True)
+
+    class Meta(UserCreationForm.Meta):
+        model = User  # type: ignore
+        fields = ('username', 'email', 'first_name', 'last_name', 'role')  # type: ignore
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data['email']
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.role = self.cleaned_data['role']
+        if commit:
+            user.save()
+        return user
+
+class CustomAuthenticationForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget = forms.EmailInput()
+        self.fields['username'].label = 'Email'
