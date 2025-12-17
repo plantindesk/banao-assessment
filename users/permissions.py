8b9fd9dec1613ab0fad43ff21f4ecaa4a217bdfc
plantindesk <t19.thakur@outlook.com>
2025-12-17 20:43:21 +0530

    feat: Add more permission for user safety

--- users/permissions.py
+++ users/permissions.py
@@ -22,3 +22,12 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.user == request.user
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow owners of an object to edit it.
+    """
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.user == request.user
