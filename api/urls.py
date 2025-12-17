4142c3cd3a8440eabda2a19073abc05165d85e20
plantindesk <t19.thakur@outlook.com>
2025-12-17 20:32:58 +0530

    feat: Add url of schedule app

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-
 router = DefaultRouter()
-
 urlpatterns = [
     path('auth/', include('users.urls', namespace='users')),
+    path('scheduling/', include('scheduling.urls', namespace='scheduling')),
     path('', include(router.urls)),
 ]
