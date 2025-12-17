9984182e8a9ae52c59a5c0ce34f647eef7d31db7
plantindesk <t19.thakur@outlook.com>
2025-12-18 03:04:38 +0530

    feat: Added google callender redirect url

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,9 @@
+from users.views import GoogleCalendarRedirectView
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('oauth2callback', GoogleCalendarRedirectView.as_view(), name='google-calendar-redirect'),
 ]
